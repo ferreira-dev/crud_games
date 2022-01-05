de3a550b07f7b70f8effef3d3278b62c508932df
Fabricio Ferreira <fabroide@gmail.com>
2022-01-05 19:39:11 -0300

    #upload - pasta correta

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
-$id= $_SESSION["id"];	
+$id = $_SESSION["id"];
 
-if(empty($id)) {
-	header ("Location: ../login.php");	
+if (empty($id)) {
+	header("Location: ../login.php");
 	exit;
 }
 ?>
 
 <h2>restrito</h2>
+<br><a href="../logout.php">Sair</a>
